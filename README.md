4a57b24b32abc9a1be35d0b9785b29c836a159f3
Aleksei Kosianenko <lehakos@gmail.com>
2022-03-16 19:45:55 +0300

    feat(chore): init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Weather App
+
+I this project I am trying to use Next.js and Tailwind.css
---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return <div className="flex min-h-screen">Home</div>
+}
+
+export default Home
